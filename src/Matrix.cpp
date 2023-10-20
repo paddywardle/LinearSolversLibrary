7c91740c0d47d312106955512de3b01846411029
Paddy Wardle <paddy.wardle@yahoo.com>
2023-10-20 09:54:05 +0100

    Matrix initial commit

---
+++ src/Matrix.cpp
@@ -0,0 +1,5 @@
+#include "Matrix.h"
+
+Matrix::Matrix(size_t numRows, size_t numCols)
+    :rows(numRows), cols(numCols) {}
+
---
+++ src/Matrix.h
@@ -0,0 +1,9 @@
+class Matrix{
+
+    private:
+        size_t rows;
+        size_t cols;
+    public:
+        Matrix(size_t numRows, size_t numCols);
+
+};
