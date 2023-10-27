45d91c043e144ee1a448248b39482dc0554e09df
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-27 20:19:35 +0100

    tests updated

--- test/test_DenseMatrix.cpp
+++ test/test_DenseMatrix.cpp
@@ -9,18 +9,33 @@ TEST(DenseMatrixTest, Data) {
 
     DenseMatrix mat(2, 3, matrix);
 
-    EXPECT_EQ(mat.getData()[0][1], 2);
-    EXPECT_EQ(mat.getData()[0][2], 3);
-    EXPECT_EQ(mat.getData()[1][0], 4);
-    EXPECT_EQ(mat.getData()[1][2], 6);
+    EXPECT_EQ(mat.getData()[1], 2);
+    EXPECT_EQ(mat.getData()[2], 3);
+    EXPECT_EQ(mat.getData()[3], 4);
+    EXPECT_EQ(mat.getData()[5], 6);
 }
 
 TEST(DenseMatrixTest, DataEmpty) {
 
     DenseMatrix mat(2, 3);
 
-    EXPECT_EQ(mat.getData()[0][1], 0);
-    EXPECT_EQ(mat.getData()[0][2], 0);
-    EXPECT_EQ(mat.getData()[1][0], 0);
-    EXPECT_EQ(mat.getData()[1][2], 0);
+    EXPECT_EQ(mat.getData()[1], 0);
+    EXPECT_EQ(mat.getData()[2], 0);
+    EXPECT_EQ(mat.getData()[3], 0);
+    EXPECT_EQ(mat.getData()[5], 0);
+}
+
+TEST(DenseMatrixTest, MatrixMultiplication) {
+
+    std::vector<std::vector<double>> matAData{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+    std::vector<std::vector<double>> matBData{{9,10,11,12}, {13,14,15,16}, {17,18,19,20}, {21,22,23,24}};
+
+    DenseMatrix matA(3, 4, matAData);
+    DenseMatrix matB(4, 4, matBData);
+    DenseMatrix mat = matA.matMul(matB);
+
+    EXPECT_EQ(mat.getData()[1], 180);
+    EXPECT_EQ(mat.getData()[5], 436);
+    EXPECT_EQ(mat.getData()[7], 488);
+    EXPECT_EQ(mat.getData()[10], 734);
 }
--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -1,11 +1,3 @@
 #include <gtest/gtest.h>
 
 #include "../src/Matrix/Matrix.h"
-
-TEST(BaseMatrixTest, RowsCols) {
-
-    Matrix mat(2, 3);
-
-    EXPECT_EQ(mat.numRows(), 2);
-    EXPECT_EQ(mat.numCols(), 3);
-}
