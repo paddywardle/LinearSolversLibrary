59305c0a33c66709130852d9d06aa8dcad439e81
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-27 20:18:48 +0100

    DenseMatrix updated

--- src/Matrix/DenseMatrix.cpp
+++ src/Matrix/DenseMatrix.cpp
@@ -1,9 +1,13 @@
-#include "DenseMatrix.h"
+#include <iostream>
 
-DenseMatrix::DenseMatrix(size_t numRows, size_t numCols, const std::vector<std::vector<double>> initialData){
+#include "DenseMatrix.h"
+#include "../Exceptions/DenseMatrixExceptions.h"
+
+DenseMatrix::DenseMatrix(const size_t numRows, const size_t numCols, const std::vector<std::vector<double>> initialData){
 
     rows = numRows;
     cols = numCols;
+    
     if (!initialData.empty()){
         // would probably be good to add a check that column and rows are equal
         // throw a custom exception here if the number of rows in the data don't match the data
@@ -13,8 +17,9 @@ DenseMatrix::DenseMatrix(size_t numRows, size_t numCols, const std::vector<std::
         }
         data_ = flattenedData;
     }
-
-    data_ = std::vector<double>(numRows*numCols, 0);
+    else {
+        data_ = std::vector<double>(numRows*numCols, 0);
+    }
 };
 
 size_t DenseMatrix::numRows() const{ 
@@ -37,20 +42,42 @@ DenseMatrix DenseMatrix::matMul(const DenseMatrix& denseMatOther) const{
     int matBRows = denseMatOther.numRows();
     int matBCols = denseMatOther.numCols();
 
-    if (matACols == matBRows){
-        // needs to be a vector of vectors
-        std::vector<std::vector<double>>resultMat (matARows,std::vector<double>(matBCols,0));
-
-        for (int i=0; i<matARows; i++){
-            for (int j=0; j<matACols; i++){
-                for (int k=0; k<matACols; i++){
-                    // make this look prettier (make sure it's correct)
-                    resultMat[i][j] += this->getData()[i*matACols+k]*denseMatOther.getData()[k*matBCols+j];
-                }
-            }
-        }
-        // need to actually put it as a vector of vector for the correct initialisation
-        return DenseMatrix(this->numRows(), denseMatOther.numCols(), resultMat);
+    if (matACols != matBRows){
+        throw DenseMatrixExceptions("Error: Matrix dimensions do not match!");
     }
 
+    const std::vector<double>& matAData = this->getData();
+    const std::vector<double>& matBData = denseMatOther.getData();
+
+    // needs to be a vector of vectors
+    std::vector<std::vector<double>>resultMat (matARows,std::vector<double>(matBCols,0));
+
+    for (int i=0; i<matARows; i++){
+        for (int j=0; j<matACols; j++){
+            for (int k=0; k<matACols; k++){
+                // make this look prettier (make sure it's correct)
+                resultMat[i][j] += matAData[i*matACols+k]*matBData[k*matBCols+j];
+            }
+        }
+    }
+    // need to actually put it as a vector of vector for the correct initialisation
+    return DenseMatrix(matARows, matBCols, resultMat);
+}
+
+std::ostream& operator<<(std::ostream& os, const DenseMatrix& denseMat){
+    
+    int matRows = denseMat.numRows();
+    int matCols = denseMat.numCols();
+
+    const std::vector<double>& matData = denseMat.getData();
+
+    for (int i=0; i<matRows; i++){
+        for (int j=0; j<matCols; j++){
+
+            os<<matData[i*matCols+j]<<" ";
+        }
+        os<<"\n";
+    }
+
+    return os;
 }
--- src/Matrix/DenseMatrix.h
+++ src/Matrix/DenseMatrix.h
@@ -2,6 +2,7 @@
 #define DENSEMATRIX_H
 
 #include <vector>
+#include <iostream>
 
 #include "Matrix.h"
 
@@ -13,15 +14,21 @@ class DenseMatrix: public Matrix {
         std::vector<double> data_;
 
     public:
-
-        DenseMatrix(size_t numRows, size_t numCols, const std::vector<std::vector<double>> data_={});
+        
+        // Instantiation
+        DenseMatrix(const size_t numRows, const size_t numCols, const std::vector<std::vector<double>> data_={});
         ~DenseMatrix(){};
         
+        // Access functions
         size_t numRows() const override;
         size_t numCols() const override;
         std::vector<double> getData() const override;
 
+        // Methods
         DenseMatrix matMul(const DenseMatrix& other) const;
+
+        // Overloads
+        friend std::ostream& operator<<(std::ostream& os, const DenseMatrix& denseMat);  
 
 };
 
