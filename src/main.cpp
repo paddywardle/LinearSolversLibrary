b8c8e12eae8e21e8937a8824918b8d0722c44710
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-28 00:07:01 +0100

    main updates

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "Matrix/DenseMatrix.h"
+#include "MatOps/DMOps.h"
 #include "Exceptions/DenseMatrixExceptions.h"
 
 int main(){
@@ -9,9 +10,9 @@ int main(){
         std::vector<std::vector<double>> matAData{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
         std::vector<std::vector<double>> matBData{{9,10,11,12}, {13,14,15,16}, {17,18,19,20}, {21,22,23,24}};
 
-        DenseMatrix matA(3, 5, matAData);
+        DenseMatrix matA(3, 4, matAData);
         DenseMatrix matB(4, 4, matBData);
-        DenseMatrix mat = matA.matMul(matB);
+        DenseMatrix mat = DMOps::matMul(matA, matB);
         std::cout<<mat;
     }
     catch (const DenseMatrixExceptions& e){
