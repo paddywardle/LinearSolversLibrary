1e20dbbaae55c22704580167944e63865f4b15aa
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-28 10:18:59 +0100

    test_vector

--- test/test_DenseMatrix.cpp
+++ test/test_DenseMatrix.cpp
@@ -4,11 +4,21 @@
 #include "../src/Matrix/DenseMatrix.h"
 #include "../src/MatOps/DMOps.h"
 
+TEST(DenseMatrixTest, numRowsCols) {
+
+    std::vector<std::vector<double>> matrix{{1, 2, 3}, {4, 5, 6}};
+
+    DenseMatrix mat(matrix);
+
+    EXPECT_EQ(mat.numRows(), 2);
+    EXPECT_EQ(mat.numCols(), 3);
+}
+
 TEST(DenseMatrixTest, Data) {
 
     std::vector<std::vector<double>> matrix{{1, 2, 3}, {4, 5, 6}};
 
-    DenseMatrix mat(2, 3, matrix);
+    DenseMatrix mat(matrix);
 
     EXPECT_EQ(mat.getData()[1], 2);
     EXPECT_EQ(mat.getData()[2], 3);
@@ -16,25 +26,15 @@ TEST(DenseMatrixTest, Data) {
     EXPECT_EQ(mat.getData()[5], 6);
 }
 
-TEST(DenseMatrixTest, DataEmpty) {
-
-    DenseMatrix mat(2, 3);
-
-    EXPECT_EQ(mat.getData()[1], 0);
-    EXPECT_EQ(mat.getData()[2], 0);
-    EXPECT_EQ(mat.getData()[3], 0);
-    EXPECT_EQ(mat.getData()[5], 0);
-}
-
 TEST(DenseMatrixTest, MatrixMultiplication) {
 
     std::vector<std::vector<double>> matAData{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
     std::vector<std::vector<double>> matBData{{9,10,11,12}, {13,14,15,16}, {17,18,19,20}, {21,22,23,24}};
 
-    DenseMatrix matA(3, 4, matAData);
-    DenseMatrix matB(4, 4, matBData);
-    DenseMatrix mat = DMOps::matMul(matA, matB);
+    DenseMatrix matA(matAData);
+    DenseMatrix matB(matBData);
 
+    DenseMatrix mat = DMOps::matMul(matA, matB);
     EXPECT_EQ(mat.getData()[1], 180);
     EXPECT_EQ(mat.getData()[5], 436);
     EXPECT_EQ(mat.getData()[7], 488);
---
+++ test/test_DenseVector.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "../src/Vector/DenseVector.h"
+#include "../src/VecOps/DVOps.h"
+
+TEST(DenseVectorTest, numRowsCols) {
+
+    std::vector<double> vector{1, 2, 3};
+
+    DenseVector vec(vector);
+
+    EXPECT_EQ(vec.getLen(), 3);
+}
+
+TEST(DenseVectorTest, Data) {
+
+    std::vector<double> vector{1, 2, 3, 4, 5, 6};
+
+    DenseVector vec(vector);
+
+    EXPECT_EQ(vec.getData()[1], 2);
+    EXPECT_EQ(vec.getData()[2], 3);
+    EXPECT_EQ(vec.getData()[3], 4);
+    EXPECT_EQ(vec.getData()[5], 6);
+}
+
+TEST(DenseVectorTest, VectorMultiplication) {
+
+    std::vector<double> vecAData{1,2,3,4};
+    std::vector<double> vecBData{9,10,11,12};
+
+    DenseVector vecA(vecAData);
+    DenseVector vecB(vecBData);
+
+    DenseVector vec = DVOps::elemWise(vecA, vecB);
+
+    EXPECT_EQ(vec.getData()[0], 9);
+    EXPECT_EQ(vec.getData()[1], 20);
+    EXPECT_EQ(vec.getData()[2], 33);
+    EXPECT_EQ(vec.getData()[3], 48);
+}
