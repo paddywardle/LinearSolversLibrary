484289d877591874c562cd675869863d3c5d79e3
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-28 09:41:03 +0100

    main updated

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main(){
         std::vector<std::vector<double>> matAData{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
         std::vector<std::vector<double>> matBData{{9,10,11,12}, {13,14,15,16}, {17,18,19,20}, {21,22,23,24}};
 
-        DenseMatrix matA(3, 4, matAData);
-        DenseMatrix matB(4, 4, matBData);
+        DenseMatrix matA(matAData);
+        DenseMatrix matB(matBData);
         DenseMatrix mat = DMOps::matMul(matA, matB);
         std::cout<<mat;
     }
