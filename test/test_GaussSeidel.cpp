3e32e358bd0b0fc15b455b49605be1cc14bd8452
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-31 13:15:31 +0000

    Tests updated

--- test/test_GaussSeidel.cpp
+++ test/test_GaussSeidel.cpp
@@ -2,20 +2,8 @@
 #include <vector>
 
 #include "../src/Matrix/DenseMatrix.h"
-#include "../src/Matrix/DenseMatrix.h"
 #include "../src/Vector/DenseVector.h"
-#include "../src/VecOps/DVOps.h"
 #include "../src/LinearSolvers/GaussSeidel.h"
-
-TEST(GaussSeidel, Residual) {
-
-    DenseMatrix mat({{1, 2, 3}, {4, 5.5, 6}, {7, 8, 9}});
-    DenseVector vec({1, 2.43, 3});
-    
-    double res = GaussSeidel::residual(mat, vec, vec);
-
-    EXPECT_EQ(res, 97.235);
-}
 
 TEST(GaussSeidel, Solver) {
 
---
+++ test/test_Residuals.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "../src/Matrix/DenseMatrix.h"
+#include "../src/Vector/DenseVector.h"
+#include "../src/Residuals/Residuals.h"
+
+TEST(Residuals, L1MatMul) {
+
+    DenseMatrix mat({{1, 2, 3}, {4, 5.5, 6}, {7, 8, 9}});
+    DenseVector vec({1, 2.43, 3});
+    
+    double res = Residuals::L1MatMul(mat, vec, vec);
+
+    EXPECT_EQ(res, 97.235);
+}
