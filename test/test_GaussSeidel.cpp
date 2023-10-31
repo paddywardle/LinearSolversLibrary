afdca365f82501b3caeac8907184952317f2f28e
Patrick Wardle <paddy.wardle@yahoo.com>
2023-10-31 00:17:52 +0000

    GaussSeidel solver test

--- test/test_GaussSeidel.cpp
+++ test/test_GaussSeidel.cpp
@@ -16,3 +16,26 @@ TEST(GaussSeidel, Residual) {
 
     EXPECT_EQ(res, 97.235);
 }
+
+TEST(GaussSeidel, Solver) {
+
+    DenseMatrix A({{5,2,1,0,0,0,0},
+                {2,6,2,1,0,0,0},
+                {1,2,7,2,1,0,0},
+                {0,1,2,8,2,1,0},
+                {0,0,1,2,9,2,1},
+                {0,0,0,1,2,10,2},
+                {0,0,0,0,1,2,11}});
+
+    DenseVector b({1,2,3,4,5,6,7});
+
+    DenseVector x = GaussSeidel::solver(A, b, 1000);
+
+    EXPECT_NEAR(x(0), 0.08082308, 1e-5);
+    EXPECT_NEAR(x(1), 0.17884866, 1e-5);
+    EXPECT_NEAR(x(2), 0.23818728, 1e-5);
+    EXPECT_NEAR(x(3), 0.28888732, 1e-5);
+    EXPECT_NEAR(x(4), 0.31639404, 1e-5);
+    EXPECT_NEAR(x(5), 0.40089018, 1e-5);
+    EXPECT_NEAR(x(6), 0.53471142, 1e-5);
+}
