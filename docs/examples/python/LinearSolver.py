72af5c386207d8121064cc6e833bf7c9c2e8bdb5
Patrick Wardle <paddy.wardle@yahoo.com>
2023-11-05 17:36:07 +0000

    docs first commit

---
+++ docs/examples/python/LinearSolver.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("python/build")
+
+import LinSol
+
+A = LinSol.DenseMatrix([
+            [10, 1, 0, 0, 0, 0, 0],
+            [1, 11, 1, 0, 0, 0, 0],
+            [0, 1, 12, 1, 0, 0, 0],
+            [0, 0, 1, 13, 1, 0, 0],
+            [0, 0, 0, 1, 14, 1, 0],
+            [0, 0, 0, 0, 1, 15, 1],
+            [0, 0, 0, 0, 0, 1, 16]
+])
+
+b = LinSol.DenseVector([1,2,3,4,5,6,7])
+
+print("Gauss Seidel")
+x = LinSol.Solvers.GS.solver(A, b)
+print(x)
+
+print("Jacobi")
+x = LinSol.Solvers.Jac.solver(A, b)
+print(x)
+
+print("Steepest Descent")
+x = LinSol.Solvers.SD.solver(A, b)
+print(x)
+
+print("Conjugate Gradient")
+x = LinSol.Solvers.CG.solver(A, b)
+print(x)
---
+++ docs/examples/python/Operations.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("python/build")
+
+import LinSol
+
+matA = LinSol.DenseMatrix([
+            [10, 1, 0, 0, 0, 0, 0],
+            [1, 11, 1, 0, 0, 0, 0],
+            [0, 1, 12, 1, 0, 0, 0],
+            [0, 0, 1, 13, 1, 0, 0],
+            [0, 0, 0, 1, 14, 1, 0],
+            [0, 0, 0, 0, 1, 15, 1],
+            [0, 0, 0, 0, 0, 1, 16]
+])
+
+vecA = LinSol.DenseVector([1, 2, 3, 4, 5, 6, 7])
+
+print("matA@matA\n\n", LinSol.DMOps.matMul(matA, matA))
+print("\n")
+print("vecA@vecA\n\n", LinSol.DVOps.dot(vecA, vecA))
+print("\n")
+print("matA@vecA\n\n", LinSol.DVOps.DVMOps.matVecMul(matA, vecA))
+print("\n")
+print("vecA@matA\n\n", LinSol.DVOps.DVMOps.vecMatMul(vecA, matA))
